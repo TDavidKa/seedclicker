cd7a4f064b37c62f3cba5dba8616778af76dea85
David K <tyler.kaufman@maine.edu>
2023-12-13 02:12:07 -0500

    upload files

---
+++ script.js
@@ -0,0 +1,142 @@
+/*Just to note, the exact numbers for a lot of these things are very much subject to 
+change. The scale on a lot of the plant prices is really wacky, but I'm not entirely 
+sure what calculations to do to tune them to ne fairer.*/
+
+var seeds = 1000000;
+var perSec = 0;
+
+//The amount of purchasable plants: Seeds, Sunflower, Tomato, Orchid, Apple, Rose
+var plants = [0, 0, 0, 0, 0, 0];
+
+//The prices of each purchasable plant.
+var prices = [50, 100, 1024, 4096, 9890, 23240];
+
+//The value each plant makes per second
+var plantVal = [0.1, 5, 25, 128, 512, 1536]
+
+function seedMouseOver(seed){
+    seed.style.width="37.5%";
+    seed.style.height="80%";
+}
+
+function seedMouseLeave(seed){
+    seed.style.width="35%";
+    seed.style.height="75%";
+}
+
+//runs every time the seed is clicked
+function seedMouseDown(seed){
+    seed.style.width="35%";
+    seed.style.height="75%";
+    //Increment the amount of seeds by one. Will likely be more in the future based on Upgrades
+    seeds++;
+    //Updates seed amount
+    document.getElementById("seedAmt").innerHTML = String(seeds);
+}
+
+function seedMouseUp(seed){
+    seed.style.width="37.5%";
+    seed.style.height="80%";
+}
+
+/*Once again, for reference,
+0: seeds
+1: sunflower
+2: tomato
+3: orchid
+4: apple
+5: rose
+*/
+function onBuy(plantNum){
+    console.log("Workin!");
+    if(seeds >= prices[plantNum]){
+        seeds -= prices[plantNum];
+        plants[plantNum]++;
+
+        //Would be easier to do *=, but this ensures we always get a whole number for 
+        //the price.
+        prices[plantNum] = Math.round(1.25 * prices[plantNum]);
+        if(plantNum == 0){
+            document.querySelector("#seedPlantCost").innerHTML = prices[0];
+            document.querySelector("#seedPlantOwned").innerHTML = plants[0];
+        }
+        else if(plantNum == 1){
+            document.querySelector("#sunPlantCost").innerHTML = prices[1];
+            document.querySelector("#sunPlantOwned").innerHTML = plants[1];
+            spawnSunflower();
+        }
+        else if(plantNum == 2){
+            document.querySelector("#tomatoPlantCost").innerHTML = prices[2];
+            document.querySelector("#tomatoPlantOwned").innerHTML = plants[2];
+            spawnTomato();
+        }
+        else if(plantNum == 3){
+            document.querySelector("#orchidPlantCost").innerHTML = prices[3];
+            document.querySelector("#orchidPlantOwned").innerHTML = plants[3];
+            spawnOrchid();
+        }
+        else if(plantNum == 4){
+            document.querySelector("#applePlantCost").innerHTML = prices[4];
+            document.querySelector("#applePlantOwned").innerHTML = plants[4];
+        }
+        else if(plantNum == 5){
+            document.querySelector("#rosePlantCost").innerHTML = prices[5];
+            document.querySelector("#rosePlantOwned").innerHTML = plants[5];
+        }
+    }
+}
+
+function seedsPerSec(){
+    /*Each plant type makes a different number of seeds per second, per plant. This 
+    multiplies the amount of each plant type by their plant multiplier (for now they're 
+    just constants, in the future it'll also be based on Upgrades 😁) */
+    perSec = plants[0] * plantVal[0] + plants[1] * plantVal[1] + plants[2] * plantVal[2] + plants[3] * plantVal[3] + plants[4] * plantVal[4] + plants[5] * plantVal[5];
+
+    seeds += perSec;
+    //https://stackoverflow.com/questions/7342957/how-do-you-round-to-one-decimal-place-in-javascript
+    //Basically just ensures that the seed amount never has more than one decimal point when above 1000.
+    if(seeds <=1000){
+        seeds = Math.round(10*seeds) / 10;
+    }
+    //if we're above 1000 seeds, tens of seeds won't really matter
+    else{
+        seeds = Math.round(seeds);
+    }
+    //Ensures that the perSec amount never has more than one decimal point when above 1000.
+    if(perSec <=1000){
+        perSec = Math.round(10*perSec) / 10;
+    }
+    //if we're above 1000 seeds per second, tens of seeds per sec won't really matter
+    else{
+        perSec = Math.round(perSec);
+    }
+
+    //Update the seed amount and seeds per second on the page.
+    document.querySelector("#seedPerSec").innerHTML = String(perSec);
+    document.querySelector("#seedAmt").innerHTML = String(seeds);
+    
+    //runs this function every second
+    setTimeout(seedsPerSec, 1000);
+}
+
+function overlayToggle(){
+    let overlay = document.querySelector("#overlay");
+    console.log(overlay.style.display);
+    if(overlay.style.display === "none" || overlay.style.display === ""){
+        overlay.style.display = "block";
+    }
+    else{
+        overlay.style.display = "none";
+    }
+}
+
+function menuToggle(){
+    let menu = document.querySelector("#menu");
+    console.log(menu.style.display);
+    if(menu.style.display === "none" || menu.style.display === ""){
+        menu.style.display = "block";
+    }
+    else{
+        menu.style.display = "none";
+    }
+}
---
+++ sketch.js
@@ -0,0 +1,268 @@
+//This page focuses specifically on the Canvas.
+
+var gardenDiv = document.getElementById("gardenSct");
+var theCanvWidth = 9 * gardenDiv.offsetWidth / 10;
+var theCanvHeight = 8 * gardenDiv.offsetHeight / 10;
+console.log(theCanvWidth + " " + theCanvHeight);
+
+//2D array of all purchasable plants: Seeds, Sunflower, Tomato, Orchid, Apple, Rose
+var plantsCanv = [[], [], [], [], [], []];
+
+function setup(){
+    var canvas = createCanvas(theCanvWidth, theCanvHeight);
+    canvas.parent('gardenCanv');
+    //Begins the "seeds per second" function
+    seedsPerSec();
+}
+
+function draw(){
+    /*clear the previous canvas, so the page doesn't get crowded by things the user 
+    can't even see*/
+    clear();
+
+    //The sky in the background
+    background('#C4E4F6');
+
+    //Hills in the background
+    fill('#6BB604');
+    noStroke();
+    circle(theCanvWidth/4, theCanvHeight/3, theCanvWidth/3);
+    circle(5*theCanvWidth/8, theCanvHeight/3, theCanvWidth/4);
+
+    //The floor
+    fill('#895A35');
+    noStroke();
+    rect(0, theCanvHeight/4, theCanvWidth, theCanvHeight);
+
+    //the rows for each plant
+    stroke("#A6754E");
+    strokeWeight(5);
+    for(let i = 0; i < 5; i++){
+        let lineHeight = (20-3*i)*theCanvHeight/20;
+        //see explanation about rows above spawnSunflower() function below
+        line(0, lineHeight, theCanvWidth, lineHeight)
+    }
+    strokeWeight(1);
+    
+    
+
+    for(var i = 0; i < plantsCanv.length; i++){
+        for(var j = 0; j < plantsCanv[i].length; j++){
+            plantsCanv[i][j].spawn();
+        }
+    }
+
+    noLoop();
+}
+
+//the 'floor' is 3/4ths of the canvas, which needs to be split into 5 segments for the 5
+//different plant types. 3/4 * 1/5 = 3/20ths, so each plant will have 3/20ths of the
+//screen to spawn in (rounded to 2/20ths, so there's some space between them)
+
+//sunflower starts at the bottom (theCanvHeight) and 2/20ths of the screen at the bottom
+var sunflowerYmin = theCanvHeight;
+var sunflowerYmax = 18 * theCanvHeight / 20;
+function spawnSunflower(){
+    //only spawns a maximum of 25 flowers, to ensure the canvas isn't entirely overtaken 
+    //by sunflowers !
+    if(plantsCanv[1].length < 26){
+        let plantX = random(0, theCanvWidth);
+        let plantY = random(sunflowerYmin, sunflowerYmax);
+        console.log(plantX + " " + plantY);
+        plantsCanv[1].push(new Sunflower(plantX, plantY)); 
+        console.log(plantsCanv[1]);
+        draw();
+    }
+}
+
+var tomatoYmin = 17 * theCanvHeight / 20;
+var tomatoYmax = 15 * theCanvHeight / 20;
+function spawnTomato(){
+    //only spawns a maximum of 25 tomato plants, to ensure the canvas isn't entirely 
+    //overtaken by them!
+    if(plantsCanv[2].length < 26){
+        let plantX = random(0, theCanvWidth);
+        let plantY = random(tomatoYmin, tomatoYmax);
+        console.log(plantX + " " + plantY);
+        plantsCanv[2].push(new Tomato(plantX, plantY)); 
+        console.log(plantsCanv[2]);
+        draw();
+    }
+}
+
+var orchidYmin =  14 * theCanvHeight / 20;
+var orchidYmax = 13 * theCanvHeight / 20;
+function spawnOrchid(){
+    //only spawns a maximum of 25 orchids, to ensure the canvas isn't entirely 
+    //overtaken by them!
+    if(plantsCanv[3].length < 26){
+        let plantX = random(0, theCanvWidth);
+        let plantY = random(orchidYmin, orchidYmax);
+        console.log(plantX + " " + plantY);
+        plantsCanv[3].push(new Orchid(plantX, plantY)); 
+        console.log(plantsCanv[3]);
+        draw();
+    }
+}
+
+//since apples are trees, they spawn in the farthest row back. they're in this spot
+//in the functions because they're purchasable before roses.
+var appleYmin =  10 * theCanvHeight / 20;
+var appleYmax = 9 * theCanvHeight / 20;
+function spawnApple(){
+    //only spawns a maximum of 10 apples, to ensure the canvas isn't entirely 
+    //overtaken by them!
+    if(plantsCanv[3].length < 11){
+        let plantX = random(0, theCanvWidth);
+        let plantY = random(orchidYmin, orchidYmax);
+        console.log(plantX + " " + plantY);
+        plantsCanv[3].push(new Orchid(plantX, plantY)); 
+        console.log(plantsCanv[3]);
+        draw();
+    }
+}
+
+var roseYmin =  12 * theCanvHeight / 20;
+var roseYmax = 11 * theCanvHeight / 20;
+function spawnRose(){
+    //only spawns a maximum of 25 roses, to ensure the canvas isn't entirely 
+    //overtaken by them!
+    if(plantsCanv[3].length < 26){
+        let plantX = random(0, theCanvWidth);
+        let plantY = random(orchidYmin, orchidYmax);
+        console.log(plantX + " " + plantY);
+        plantsCanv[3].push(new Orchid(plantX, plantY)); 
+        console.log(plantsCanv[3]);
+        draw();
+    }
+}
+
+function changeMenuColor(obj){
+    obj.style.background = "#FAEFD4";
+}
+function menuColorBack(obj){
+    obj.style.background = "#FFEBBD";
+}
+
+class Sunflower{
+    constructor(x, y){
+        this.x = x;
+        this.y = y;
+    }
+
+    //https://editor.p5js.org/p5/sketches/hhu8mAXJpQ7 for flower shape
+    spawn(){
+        scale(0.5);
+        //petals
+        fill("#EDD400");
+        translate(this.x*2, this.y*2);
+        noStroke();
+        
+        for (let i = 0; i < 10; i ++) {
+          ellipse(0, 0, theCanvWidth/37, theCanvHeight/5);
+          rotate(PI/5);
+        }
+
+        //center circle of flowers
+        translate(-this.x*2, -this.y*2);
+        fill("#8F5902");
+        circle(this.x*2, this.y*2, theCanvHeight/10);
+        console.log(this.x + " " + this.y);
+        scale(2);
+    }
+}
+
+class Tomato{
+    constructor(x, y){
+        this.x = x;
+        this.y = y;
+    }
+
+    
+    spawn(){
+        noFill();
+        ellipseMode(CENTER);
+
+        //plant stem
+        strokeWeight(3);
+        stroke("#6A9955");
+        let radius = theCanvHeight/16;
+
+        arc(this.x, this.y - radius/2, radius, radius, HALF_PI, 3*HALF_PI);
+        arc(this.x, this.y - 3*radius/2, radius, radius, 3*HALF_PI, HALF_PI);
+
+        let theLeaf = new Leaf(this.x + radius/2, this.y - 2*radius, radius, 5*PI/4);
+        theLeaf.spawn();
+        let theLeaf2 = new Leaf(this.x - 3*radius/4, this.y - 3*radius/4, radius, 5*PI/6);
+        theLeaf2.spawn();
+        
+        //wooden stake for the tomato plant
+        stroke("#E8D9BF");
+        strokeWeight(5);
+        line(this.x, this.y, this.x, this.y - theCanvHeight/8);
+
+        strokeWeight(1);
+    }
+}
+
+class Orchid{
+    constructor(x, y){
+        this.x = x;
+        this.y = y;
+    }
+    
+    spawn(){
+        noFill();
+        strokeWeight(4);
+        stroke("#6A9955");
+        let x1 = this.x;
+        let y1 = this.y;
+        let x2 = this.x;
+        let y2 = y1 - height/8;
+        let x3 = this.x;
+        let y3 = this.y - height/8;
+        let x4 = this.x - width/26;
+        let y4 = this.y - height/10;
+        bezier(x1, y1, x2, y2, x3, y3, x4, y4);
+        strokeWeight(1);
+        
+        //the "flowers" on the orchid branch
+        //I took....... artistic liberties, on a lot of the numbers here. definitely
+        //could be a lot better with dynamic generation, but I didn't want to
+        //micromanage even *more* numbers in this one spawn function.
+        //I didn't mean for it to look like grapes. oops!
+        fill("#DB67CF");
+        stroke("#DB67CF");
+        let circleR = theCanvHeight/50;
+        circle(x4, y4+circleR/2, circleR);
+        circle(x4+circleR, y4+circleR/2-2, circleR);
+        circle(x4+circleR*2, y4+circleR/2-4, circleR);
+        circle(x4+3*circleR/2, y4+circleR/2+3, circleR);
+        circle(x4+circleR/2, y4+circleR/2+6, circleR);
+        circle(x4+circleR, y4+circleR/2+10, circleR);
+        
+    }
+}
+
+// Rotations are shockingly hard to use in p5, so I had to make a Leaf class for the
+//tomato plant art
+class Leaf{
+    constructor(x, y, r, rotate){
+        this.x = x;
+        this.y = y;
+        this.r = r/2;
+        this.rotate = rotate;
+    }
+
+    spawn(){
+        fill("#6A9955");
+        translate(this.x, this.y);
+        rotate(this.rotate);
+        
+        ellipse(0, 0, this.r/4, this.r);
+        
+        rotate(-this.rotate);
+        translate(-this.x, -this.y);
+        noFill();
+    }
+}
